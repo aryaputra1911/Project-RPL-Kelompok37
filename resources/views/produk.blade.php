d8acbf47212b353f92174b6fd85fcc3027a84d57
aryaputa1911 <aryaputraa1911@gmail.com>
2026-05-18 16:27:34 +0700

    benerin clode button

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -292,7 +292,11 @@
 
     <div class="bg-white w-[1000px] rounded-xl overflow-hidden relative flex">
 
-        <button onclick="closeModal()" class="absolute top-4 right-4 text-xl">&times;</button>
+        <button onclick="closeModal()" class="absolute top-4 right-4 p-1 text-gray-500 hover:text-gray-800 transition" aria-label="Tutup">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+            </svg>
+        </button>
 
         <div class="w-1/2">
             <img id="modalImg" class="w-full h-full object-cover">
